7e7890887a7702301f9302c351cb7e2f58123573
aremo90 <mrhamza2005@gmail.com>
2025-08-25 06:59:01 +0300

    Question 1

---
+++ LibraryEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, BookDelegate fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+        
+        public static void ProcessBooks(List<Book> bList, Func<Book, string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> books = new List<Book>();
+
+            books.Add(new Book("978-3-16-148410-0", "C# Programming", new string[] { "Alice Smith", "Bob Johnson" },new DateTime(2020, 5, 1), 49.99m));
+            books.Add(new Book("978-1-23-456789-0", "Advanced C#", new string[] { "Charlie Brown" }, new DateTime(2021, 3, 15), 59.99m));
+
+
+            Console.WriteLine("User Defined Delegate Datatype");
+            LibraryEngine.ProcessBooks(books, new BookDelegate(BookFunctions.GetTitle));
+
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("BCL Delegate");
+            Func<Book, string> func = B => string.Join(", ", B.Authors);
+            LibraryEngine.ProcessBooks(books, func);
+
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("Anonymous Method (GetISBN)");
+            Func<Book, string> anon = delegate (Book B) { return B.ISBN; };
+            LibraryEngine.ProcessBooks(books, anon);
+
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("Lambda Expression (GetPublicationDate)");
+            Func<Book, string> lambda = B => B.PublicationDate.ToShortDateString();
+            LibraryEngine.ProcessBooks(books, lambda);
         }
     }
 }
