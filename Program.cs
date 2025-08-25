1551361b5217400e5c445d1d0748acdcab4ede54
aremo90 <mrhamza2005@gmail.com>
2025-08-25 05:06:58 +0300

    01 Delegate

--- Program.cs
+++ Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate
+
+            /*
+             * Delegarte:-
+             *      - C# language Feature
+             *      - Implement Functional Programming paradigm
+             *      - Implement Event driven programming
+             *      - Enforce type safety
+             *      - Reference of merthod
+             *          object of delegate can store Reference of method or More one method
+             *      - Delegate is multicast
+             *          can refer to multiple methods
+             */
+
+
+
+            #endregion
+
+            #region Example 1
+
+            // Step 1 : Declare reference from delegate
+            StringFuncDelegate stringCase;
+
+            // Step 2
+            stringCase = new StringFuncDelegate(StringFunc.CountOfUpperCase);
+            // stringUpperCase = StringFunc.CountOfUpperCase; => This is also valid
+
+            // Add new method
+            //stringCase += StringFunc.CountOfLowerCase;
+
+            // Remove method
+            //stringCase -= StringFunc.CountOfLowerCase;
+
+
+            //Console.WriteLine(stringCase("Hello")); => Output : 1 Upper Case
+
+            int result = stringCase.Invoke("Hello WorlD"); // Output : 3 Upper Case
+            Console.WriteLine(result);
+            // Note : Invoke always return the result of the last method
+
+            #endregion
+
+
         }
     }
 }
